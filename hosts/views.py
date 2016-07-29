b11ac37d363bdbf975d75ca74960ab4ed112a3c7
Alex hao <75898234@qq.com>
2016-07-29 15:18:30 +0800

    完成主机模块主页面

--- hosts/views.py
+++ hosts/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+import models
 # Create your views here.
 
 @login_required
 def hosts_index(request):
-    return render(request, "hosts/hosts.html")
+    selected_gid = request.GET.get('selected_gid')
+    if selected_gid:
+        host_list = models.BindHostToGroup.objects.get(host_group=selected_gid).bind_hosts.all()
+    else:
+        host_list = request.user.bind_hosts.select_related()
+    return render(request, "hosts/hosts.html",{'host_list':host_list})
 
 @login_required
 def hosts_files(request):
